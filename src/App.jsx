c1f1af0014bdf63fa8f3dd731ef27e768f8049b1
Yazan <yazanjarbou077@gmail.com>
2023-01-11 12:07:38 +0100

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ResumeForm from "./components/ResumeForm"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeForm/>
+    </div>
+  );
+}
+
+export default App;
