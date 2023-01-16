22b37970316c5b47e637f5c9f8da155c07ade5e3
David González García <davidgonzalez@gmx.com>
2023-01-17 00:42:09 +0100

    StepperBar and ButtonBar integrated

---
+++ src/components/EducationStep/EducationStep.jsx
@@ -0,0 +1,11 @@
+import StepperBar from './components/BaseStepper/BaseStepper.jsx'
+import EducationForm from './EducationForm/EducationForm.jsx'
+import ButtonBar from './components/ButtonBar/ButtonBar.jsx'
+
+const EducationStep = () => {
+    <>
+    <StepperBar/>
+    <EducationForm/>
+    <ButtonBar/>
+    </>
+}
